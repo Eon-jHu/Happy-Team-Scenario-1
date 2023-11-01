e982fe35bf4b8c9f96eb2f682627f768568a5419
Lisa Patricia Kelly <lisa.kelly1@MDS.ac.nz>
2023-11-01 22:05:57 +1300

    Created Person Class

---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    // variables.
+    private Vector2 m_Position;
+    private Sprite m_Sprite;
+
+    // setters and getters.
+    public Vector2 GetPosition()
+    {
+        return m_Position;
+    }
+    
+    public void SetPosition(Vector2 _Position)
+    {
+        m_Position = _Position;
+    }
+
+    //public Vector2 Move()
+    //{
+    //    return; //??
+    //}
+
+    public Sprite GetSprite()
+    {
+        return m_Sprite;
+    }
+}
