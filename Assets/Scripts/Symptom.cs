e7322d2f4f0a3446d7d60d0bb267a74d1842986d
CTherond505 <caleb.therond@mds.ac.nz>
2023-11-02 15:02:09 +1300

    Saxophone Complete
    music time

---
+++ Assets/Scripts/Symptom.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Symptom
+{
+    private string symptomName;
+
+    public abstract Animation getSymptomAnimation();
+}
